4d677178c1b2a12c9af7d7b46ec26f03788df76a
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2023-12-13 20:19:31 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,87 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <!-- A div with container id to hold the cards -->
+  <div id="container">
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    <!-- A div with card class for the first card -->
+    <div class="card">
+      
+        <!-- A dummy avatar image -->
+        <img src="https://i.pravatar.cc/300?img=1" alt="Avatar">
+      
+        <!-- A div with card__details class to hold the details in the card -->
+        <div class="card__details">
 
-      <Footer />
+          <!-- Span with tag class for the tag -->
+          <span class="tag">Nature</span>
+          <span class="tag">Lake</span>
+
+          <!-- A div with name class for the name of the card -->
+          <div class="name">Lago di Braies</div>
+
+          <!-- A paragraph with some dummy text -->
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur sodales morbi dignissim sed diam pharetra vitae ipsum odio.</p>
+
+          <!-- A button element -->
+          <button>Read more</button>
+
+        </div>
+      
     </div>
+
+     <!-- A div with card class for the second card -->
+     <div class="card">
+      
+        <!-- A dummy avatar image -->
+        <img src="https://i.pravatar.cc/300?img=2" alt="Avatar">
+      
+        <!-- A div with card__details class to hold the details in the card -->
+        <div class="card__details">
+
+          <!-- Span with tag class for the tag -->
+          <span class="tag">Travel</span>
+          <span class="tag">City</span>
+
+          <!-- A div with name class for the name of the card -->
+          <div class="name">New York</div>
+
+          <!-- A paragraph with some dummy text -->
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur sodales morbi dignissim sed diam pharetra vitae ipsum odio.</p>
+
+          <!-- A button element -->
+          <button>Read more</button>
+
+        </div>
+      
+    </div>
+
+     <!-- A div with card class for the third card -->
+     <div class="card">
+      
+        <!-- A dummy avatar image -->
+        <img src="https://i.pravatar.cc/300?img=3" alt="Avatar">
+      
+        <!-- A div with card__details class to hold the details in the card -->
+        <div class="card__details">
+
+          <!-- Span with tag class for the tag -->
+          <span class="tag">Food</span>
+          <span class="tag">Pizza</span>
+
+          <!-- A div with name class for the name of the card -->
+          <div class="name">Pizza Margherita</div>
+
+          <!-- A paragraph with some dummy text -->
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur sodales morbi dignissim sed diam pharetra vitae ipsum odio.</p>
+
+          <!-- A button element -->
+          <button>Read more</button>
+
+        </div>
+      
+    </div>
+    
+  </div>
   )
 }
